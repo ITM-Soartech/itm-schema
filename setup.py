e6dbd20716c9edddcf289671591114e0ef48f964
Nick Paul <nicholas.paul@soartech.com>
2023-11-07 16:48:08 -0500

    Add python package files

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name='itm_schema',
+        version='0.0.1',
+        url='n/a',
+        author='SoarTech',
+        author_email='n/a',
+        description='In the moment TA1 schema',
+        packages=find_packages('src'),    
+        package_dir={'': 'src'},
+        install_requires=[
+            'pydantic',
+        ]
+    )
+
