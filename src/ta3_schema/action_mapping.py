fd5937920cd12119ffd36b4f3b057e77ce4c9c2a
nicholas.paul <nicholas.paul@soartech.com>
2024-05-04 15:23:51 +0000

    add optional equipment to action_mapping

--- src/ta3_schema/action_mapping.py
+++ src/ta3_schema/action_mapping.py
@@ -35,6 +35,7 @@ class ActionMapping(BaseModel):
     unstructured: StrictStr = Field(description="Natural language, plain text description of the action")
     repeatable: Optional[StrictBool] = Field(default=False, description="Whether or not this action should remain after it's selected by an ADM")
     character_id: Optional[StrictStr] = Field(default=None, description="The ID of the character being acted upon")
+    equipment_used: Optional[Dict[str, int]] = Field(default=None, description="Equipment used for this action")
     parameters: Optional[Dict[str, StrictStr]] = Field(default=None, description="key-value pairs containing additional [action-specific parameters](https://github.com/NextCenturyCorporation/itm-evaluation-client?tab=readme-ov-file#available-actions)")
     probe_id: StrictStr = Field(description="A valid probe_id from the appropriate TA1")
     choice: StrictStr = Field(description="A valid choice for the specified probe_id")
@@ -42,7 +43,7 @@ class ActionMapping(BaseModel):
     kdma_association: Optional[Dict[str, Union[Annotated[float, Field(le=1.0, strict=True, ge=0.0)], Annotated[int, Field(le=1, strict=True, ge=0)]]]] = Field(default=None, description="KDMA associations for this choice, if provided by TA1")
     condition_semantics: Optional[SemanticTypeEnum] = None
     conditions: Optional[Conditions] = None
-    __properties: ClassVar[List[str]] = ["action_id", "action_type", "unstructured", "repeatable", "character_id", "parameters", "probe_id", "choice", "next_scene", "kdma_association", "condition_semantics", "conditions"]
+    __properties: ClassVar[List[str]] = ["action_id", "action_type", "unstructured", "repeatable", "character_id", "equipment_used", "parameters", "probe_id", "choice", "next_scene", "kdma_association", "condition_semantics", "conditions"]
 
     model_config = {
         "populate_by_name": True,
@@ -103,6 +104,7 @@ class ActionMapping(BaseModel):
             "unstructured": obj.get("unstructured"),
             "repeatable": obj.get("repeatable") if obj.get("repeatable") is not None else False,
             "character_id": obj.get("character_id"),
+            "equipment_used": obj.get("equipment_used"),
             "parameters": obj.get("parameters"),
             "probe_id": obj.get("probe_id"),
             "choice": obj.get("choice"),
